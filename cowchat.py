9eae94fc9fc4a64af887395f67bef879618af6bc
Ftor_PT <15602002604@163.com>
2026-09-23 18:20:19 +0100

    Step 3: Added AI-powered responses using transformers

---
+++ cowchat.py
@@ -0,0 +1,32 @@
+import cowsay
+from transformers import pipeline
+from transformers.utils import logging
+
+# Te remove unnecessary warnings from the transformer library
+logging.set_verbosity_error()
+
+gen = pipeline("text-generation", model="HuggingFaceTB/SmolLM2-135M-Instruct")
+
+cowsay.cow("Hello! Moo!")
+
+while True:
+    input_text = input("Chat, or type 'quit': ")
+    if input_text.lower() == "quit":
+        cowsay.cow("Goodbye! Moo! Moo!")
+        break
+
+    messages = [
+        {
+            "role": "system",
+            "content": "Pretend you are a friendly cow. Answer briefly, keeping the conversation in a context that matches the life of a cow. End your response with Moo!",
+        },
+        {
+            "role": "user",
+            "content": input_text,
+        },
+    ]
+
+    response = gen(messages, max_new_tokens=100, clean_up_tokenization_spaces=False)
+    response_text = response[0]['generated_text'][-1]["content"]
+
+    cowsay.cow(response_text + " Moo!")
